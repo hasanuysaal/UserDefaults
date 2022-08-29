6cc9f4ee664f25f765fb35688a4b394087b1b1d8
hasanuysaal <hsnuysaal@icloud.com>
2022-08-29 15:15:42 +0300

    first commit

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toDoText: UITextField!
+    @IBOutlet weak var dateText: UITextField!
+    @IBOutlet weak var toDoLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let savedToDo = UserDefaults.standard.object(forKey: "toDoTextUD")
+        let savedDate = UserDefaults.standard.object(forKey: "dateTextUD")
+        
+        if let sendToDo = savedToDo as? String{
+            
+            toDoLabel.text = sendToDo
+            
+        }
+        
+        if let sendDate = savedDate as? String {
+            
+            dateLabel.text = sendDate
+            
+        }
+        
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(toDoText.text!, forKey: "toDoTextUD")
+        UserDefaults.standard.set(dateText.text!, forKey: "dateTextUD")
+        
+        toDoLabel.text = toDoText.text
+        dateLabel.text = dateText.text
+        
     }
 
-
+    @IBAction func deleteButton(_ sender: Any) {
+        
+    }
+    
 }
 
